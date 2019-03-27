5de6c94cf3dae0201b23cbda80eff660d246cca7
ibrahim ates <ates.ibrahimhalil@gmail.com>
2019-03-27 13:21:52 +0300

    Update ProcessController.java

--- src/main/java/com/ates/moneytransfer/controller/ProcessController.java
+++ src/main/java/com/ates/moneytransfer/controller/ProcessController.java
@@ -1,18 +1,13 @@
 package com.ates.moneytransfer.controller;
 
-import static spark.Spark.delete;
 import static spark.Spark.get;
 import static spark.Spark.port;
-import static spark.Spark.post;
-import static spark.Spark.put;
 
 import java.math.BigDecimal;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
 
 import com.ates.moneytransfer.service.AccountService;
 import com.ates.moneytransfer.service.TransferService;
-import com.ates.moneytransfer.util.JsonUtil;
 import com.ates.moneytransfer.model.Account;
 import com.ates.moneytransfer.model.Transfer;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -21,40 +16,46 @@ import com.google.gson.GsonBuilder;
 import spark.Spark;
 
 public class ProcessController {
-	
+
     private static ObjectMapper om = new ObjectMapper();
-    
-    private static final AtomicInteger COUNTER = new AtomicInteger();
-	
-	public ProcessController() {
+
+    private String warningMsg = "account with id ";
+    private String pathAccountWithId = "/api/accounts/:id";
+    private String pathAccountWithoutId = "/api/accounts";
+    private String pathTransfertWithId = "/api/transfers/:id";
+    private String pathTransfertWithoutId = "/api/transfers";
+
+    public ProcessController()
+    {
+        processExecute();
+    }
+
+    public void processExecute(){
 
         final AccountService accountService = new AccountService();
 
-		
-		 // Start embedded server at this port
+        // Start embedded server at this port
         port(8282);
 
-
         final TransferService transferService = new TransferService();
- 
+
         // Main Page, welcome
         get("/", (request, response) -> "Welcome money transfer application / created by Ibrahim Ates");
 
- 
+
         // GET - get account with this id
-        Spark.get("/api/accounts/:id", (request, response) -> {
+        Spark.get(pathAccountWithId, (request, response) -> {
             Account account = accountService.findById(Integer.valueOf(request.params(":id")));
             if (account != null) {
                 return om.writeValueAsString(account);
             } else {
                 response.status(404); // 404 Not found
-                return om.writeValueAsString("account with id " + Integer.valueOf(request.params(":id") + " is not found "));
+                return om.writeValueAsString(warningMsg + Integer.valueOf(request.params(":id") + " is not found "));
             }
         });
- 
+
         // Get - list all accounts
-        
-        Spark.get("/api/accounts", (request, response) -> {
+        Spark.get(pathAccountWithoutId, (request, response) -> {
             List result = accountService.findAll();
             if (result.isEmpty()) {
                 return om.writeValueAsString("accounts not found");
@@ -64,10 +65,10 @@ public class ProcessController {
         });
 
         // POST - Add an account
-        Spark.post("/api/accounts", (request, response) -> {
+        Spark.post(pathAccountWithoutId, (request, response) -> {
             Gson gson = new GsonBuilder().create();
             Account account2 = gson.fromJson(request.body() , Account.class);
-            if(account2.getName()!=null&account2.getBalance().compareTo(BigDecimal.ZERO)>=0)
+            if(account2.getName()!=null&&account2.getBalance().compareTo(BigDecimal.ZERO)>=0)
             {
                 accountService.addAccount(account2);
                 response.status(201); // 201 Created
@@ -84,48 +85,45 @@ public class ProcessController {
                 else if(account2.getBalance().compareTo(BigDecimal.ZERO)<0)
                 {
                     return om.writeValueAsString("account's balance can not be less than 0");
-
                 }
 
             }
-
             return om;
         });
- 
+
         // PUT - Update account
-        Spark.put("/api/accounts/:id", (request, response) -> {
-            int id = Integer.valueOf(request.params(":id"));
+        Spark.put(pathAccountWithId, (request, response) -> {
+            int id = Integer.parseInt(request.params(":id"));
             Account account = accountService.findById(Integer.valueOf(request.params(":id")));
             if (account != null) {
-            	
-            	Gson gson = new GsonBuilder().create();
+
+                Gson gson = new GsonBuilder().create();
                 Account account2 = gson.fromJson(request.body() , Account.class);
 
-
-                //account=accountService.updateAccount(id, account2);
                 return om.writeValueAsString(accountService.updateAccount(id, account2));
             } else {
                 response.status(404);
-                return om.writeValueAsString("account with id " + id + " is not found to update");
+                return om.writeValueAsString(warningMsg + id + " is not found to update");
             }
         });
- 
+
         // DELETE - delete account
-        Spark.delete("/api/accounts/:id", (request, response) -> {
-        	 int id = Integer.valueOf(request.params(":id"));
-             Account account = accountService.findById(Integer.valueOf(request.params(":id")));
-             if (account != null) {
+        Spark.delete(pathAccountWithId, (request, response) -> {
+            int id = Integer.parseInt(request.params(":id"));
+            Account account = accountService.findById(Integer.valueOf(request.params(":id")));
+            if (account != null) {
                 accountService.delete(id);
-                return om.writeValueAsString("account with id " + id + " is deleted!");
+                return om.writeValueAsString(warningMsg + id + " is deleted!");
             } else {
                 response.status(404);
-                return om.writeValueAsString("account with id " + id + " is not found to delete!");
+
+                return om.writeValueAsString(warningMsg + id + " is not found to delete!");
             }
         });
 
 
         // POST - Add an transfer
-        Spark.post("/api/transfers", (request, response) -> {
+        Spark.post(pathTransfertWithoutId, (request, response) -> {
             Gson gson = new GsonBuilder().create();
             Transfer transfer = gson.fromJson(request.body() , Transfer.class);
 
@@ -133,34 +131,20 @@ public class ProcessController {
             if(transfer2.getStatus()== Transfer.TransferStatus.CREATED)
             {
                 response.status(201); // 201 Created
-                om.writeValueAsString(transfer);
+                return om.writeValueAsString(transfer);
             }
             else
             {
                 response.status(400); // bad request
-                if(transfer2.getStatus()== Transfer.TransferStatus.AMOUNTZERO) {
-                    om.writeValueAsString("Because transfer amount is zero, process can not be proceed. Please check your information!");
-                }
-                else if(transfer2.getStatus()== Transfer.TransferStatus.AMOUNTLESS){
-                    om.writeValueAsString("Balance is less than amount you want send. Please check your information!");
 
-                }
-                else if(transfer2.getStatus()== Transfer.TransferStatus.FROMIDNULL){
-                    om.writeValueAsString("id that you send money can not be null. Please check your information!");
-                }
-                else if(transfer2.getStatus()== Transfer.TransferStatus.TOIDNULL){
-                    om.writeValueAsString("sender id can not be null. Please check your information!");
-                }
-
+                return om.writeValueAsString(transfer2.getStatus());
 
             }
-            return om;
-
 
         });
 
         // GET - get transfer with this id
-        Spark.get("/api/transfers/:id", (request, response) -> {
+        Spark.get(pathTransfertWithId, (request, response) -> {
             Transfer transfer = transferService.findById(Integer.valueOf(request.params(":id")));
             if (transfer != null) {
                 return om.writeValueAsString(transfer);
@@ -172,9 +156,10 @@ public class ProcessController {
 
         // Get - list all transfers
 
-        Spark.get("/api/transfers", (request, response) -> {
+        Spark.get(pathTransfertWithoutId, (request, response) -> {
             List result = transferService.findAll();
             if (result.isEmpty()) {
+
                 return om.writeValueAsString("No transfers done yet");
             } else {
                 return om.writeValueAsString(transferService.findAll());
@@ -183,8 +168,9 @@ public class ProcessController {
         });
 
         // PUT - Update transfer
-        Spark.put("/api/transfers/:id", (request, response) -> {
-            int id = Integer.valueOf(request.params(":id"));
+        Spark.put(pathTransfertWithId, (request, response) -> {
+            int id = Integer.parseInt(request.params(":id"));
+
             Transfer transfer = transferService.findById(Integer.valueOf(request.params(":id")));
             if (transfer != null) {
 
@@ -195,34 +181,20 @@ public class ProcessController {
                 if(transfer3.getStatus()== Transfer.TransferStatus.UPDATED)
                 {
                     response.status(200); // 200 Ok
-                    om.writeValueAsString(transfer3);
+                    return om.writeValueAsString(transfer3);
                 }
                 else
                 {
                     response.status(400); //
-                    if(transfer2.getStatus()== Transfer.TransferStatus.AMOUNTZERO) {
-                        om.writeValueAsString("Because transfer amount is zero, process can not be proceed. Please check your information!");
-                    }
-                    else if(transfer2.getStatus()== Transfer.TransferStatus.AMOUNTLESS){
-                        om.writeValueAsString("Balance is less than amount you want send. Please check your information!");
-
-                    }
-                    else if(transfer2.getStatus()== Transfer.TransferStatus.FROMIDNULL){
-                        om.writeValueAsString("id that you send money can not be null. Please check your information!");
-                    }
-                    else if(transfer2.getStatus()== Transfer.TransferStatus.TOIDNULL){
-                        om.writeValueAsString("sender id can not be null. Please check your information!");
-                    }
-
+                    return om.writeValueAsString(transfer2.getStatus());
 
                 }
-                return om;
-
             } else {
                 response.status(404);
                 return om.writeValueAsString("Any transfer wants to update not found");
             }
         });
+
 
     }
 
